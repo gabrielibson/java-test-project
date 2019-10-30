4ce5e08f698f080b594918a2433f9f6424687944
gabrielibson <gis@cin.ufpe.br>
2019-10-30 18:22:37 +0000

    Removing TODO lines

--- src/main/java/com/thoughtworks/hotel/api/model/ReservationRegular.java
+++ src/main/java/com/thoughtworks/hotel/api/model/ReservationRegular.java
@@ -7,24 +7,23 @@ public class ReservationRegular extends Reservation{
 
 	public ReservationRegular(ClientType clientType, List<Calendar> dates) {
 		super(clientType, dates);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public Double calculatePrice(Hotel hotel) {
 		String day = "";
-		double price = 0.0;		
+		double price = 0.0;
 		for(Calendar date : this.getDates()) {
 			//Method implemented by default method of IDateHelper
 			day = getDayByDate(date);
-			
+
 			switch(day) {
-				case "Saturday": 
+				case "Saturday":
 				case "Sunday":
 					price += hotel.getWeekendsRegularPrice();
 					break;
-				
-				default: 
+
+				default:
 					price += hotel.getWeekdaysRegularPrice();
 				}
 		}
